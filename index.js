fd68ddf6ef2b547ce7fdff24772d64336a6eb8c5
thiago0309 <thiagovsl001@gmail.com>
2021-09-12 13:47:27 -0300

    versao 1.0

---
+++ index.js
@@ -0,0 +1,51 @@
+var n1 // variável número 1
+var n2 // variável número 2
+var resultado // variável resultado
+
+function somar() {
+    /* Entrada de Dados */
+    n1 = Number(document.MinhaCalculadora.n1.value);
+    n2 = Number(document.MinhaCalculadora.n2.value);
+
+    /* Processamento de Dados */
+    resultado = n1 + n2;
+
+    /*Saída de Dados */
+    window.alert(`O Resultado é ${resultado.toString()}`)
+}
+
+function subtrair() {
+    /* Entrada de Dados */
+    n1 = Number(document.MinhaCalculadora.n1.value);
+    n2 = Number(document.MinhaCalculadora.n2.value);
+
+    /* Processamento de Dados */
+    resultado = n1 - n2;
+
+    /*Saída de Dados */
+    window.alert(`O Resultado é ${resultado.toString()}`)
+}
+
+function multiplicação() {
+    /* Entrada de Dados */
+    n1 = Number(document.MinhaCalculadora.n1.value);
+    n2 = Number(document.MinhaCalculadora.n2.value);
+
+    /* Processamento de Dados */
+    resultado = n1 * n2;
+
+    /*Saída de Dados */
+    window.alert(`O Resultado é ${resultado.toString()}`)
+}
+
+function divisão() {
+    /* Entrada de Dados */
+    n1 = Number(document.MinhaCalculadora.n1.value);
+    n2 = Number(document.MinhaCalculadora.n2.value);
+
+    /* Processamento de Dados */
+    resultado = n1 / n2;
+
+    /*Saída de Dados */
+    window.alert(`O Resultado é ${resultado.toString()}`)
+}
